b69f33dc72a7996fc4bd09434ef7b462be5b0b89
nickgou-nuke <nikolay.v.goutev@gmail.com>
2026-02-13 08:51:21 +0200

    modified:   lean/InfoGeometry.lean

--- lean/.lake/build/ir/InfoGeometry.c
+++ lean/.lake/build/ir/InfoGeometry.c
@@ -1,6 +1,6 @@
 // Lean compiler output
 // Module: InfoGeometry
-// Imports: public import Init public import InfoGeometry.Basic public import Mathlib
+// Imports: public import Init public import InfoGeometry.Basic public import Mathlib public import Mathlib.Probability.Distributions.Poisson
 #include <lean/lean.h>
 #if defined(__clang__)
 #pragma clang diagnostic ignored "-Wunused-parameter"
@@ -13,10 +13,33 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_spectralEpsilon___boxed(lean_object*, lean_object*, lean_object*);
+lean_object* lp_mathlib_SubNegZeroMonoid_toNegZeroClass___redArg(lean_object*);
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_hessianIndefiniteForm(lean_object*, lean_object*, lean_object*, lean_object*, lean_object*);
+lean_object* lp_mathlib_LinearMap_comp___redArg(lean_object*, lean_object*);
 LEAN_EXPORT lean_object* lp_info_x2dgeometry_totalMass___redArg(lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_spectralEpsilon___redArg___lam__0(lean_object*, lean_object*);
+static lean_object* lp_info_x2dgeometry_modularJ___closed__0;
 extern lean_object* lp_mathlib_Nat_instAddCancelCommMonoid;
 LEAN_EXPORT lean_object* lp_info_x2dgeometry_totalMass___redArg___lam__0(lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_modularJ(lean_object*, lean_object*, lean_object*);
+lean_object* lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Data_Real_Basic_1138242547____hygCtx___hyg_8_(lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_spectralEpsilon___redArg___boxed(lean_object*);
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_hessianIndefiniteForm___redArg(lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_complexI(lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_clmComm___redArg(lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_complexI___redArg___boxed(lean_object*);
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_modularJ___boxed(lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_hessianIndefiniteForm___boxed(lean_object*, lean_object*, lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_clmComm___boxed(lean_object*, lean_object*, lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_spectralEpsilon(lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_complexI___redArg(lean_object*);
 lean_object* lp_mathlib_Finset_sum___redArg(lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_spectralEpsilon___redArg(lean_object*);
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_modularJ___lam__0(lean_object*);
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_complexI___boxed(lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_clmComm___redArg___lam__0(lean_object*, lean_object*, lean_object*, lean_object*);
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_clmComm(lean_object*, lean_object*, lean_object*, lean_object*, lean_object*);
 LEAN_EXPORT lean_object* lp_info_x2dgeometry_totalMass(lean_object*, lean_object*, lean_object*);
 LEAN_EXPORT lean_object* lp_info_x2dgeometry_totalMass___redArg___lam__0(lean_object* x_1, lean_object* x_2) {
 _start:
@@ -45,9 +68,296 @@ x_4 = lp_info_x2dgeometry_totalMass___redArg(x_2, x_3);
 return x_4;
 }
 }
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_modularJ___lam__0(lean_object* x_1) {
+_start:
+{
+uint8_t x_2; 
+x_2 = !lean_is_exclusive(x_1);
+if (x_2 == 0)
+{
+lean_object* x_3; lean_object* x_4; 
+x_3 = lean_ctor_get(x_1, 0);
+x_4 = lean_ctor_get(x_1, 1);
+lean_ctor_set(x_1, 1, x_3);
+lean_ctor_set(x_1, 0, x_4);
+return x_1;
+}
+else
+{
+lean_object* x_5; lean_object* x_6; lean_object* x_7; 
+x_5 = lean_ctor_get(x_1, 0);
+x_6 = lean_ctor_get(x_1, 1);
+lean_inc(x_6);
+lean_inc(x_5);
+lean_dec(x_1);
+x_7 = lean_alloc_ctor(0, 2, 0);
+lean_ctor_set(x_7, 0, x_6);
+lean_ctor_set(x_7, 1, x_5);
+return x_7;
+}
+}
+}
+static lean_object* _init_lp_info_x2dgeometry_modularJ___closed__0() {
+_start:
+{
+lean_object* x_1; 
+x_1 = lean_alloc_closure((void*)(lp_info_x2dgeometry_modularJ___lam__0), 1, 0);
+return x_1;
+}
+}
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_modularJ(lean_object* x_1, lean_object* x_2, lean_object* x_3) {
+_start:
+{
+lean_object* x_4; 
+x_4 = lp_info_x2dgeometry_modularJ___closed__0;
+return x_4;
+}
+}
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_modularJ___boxed(lean_object* x_1, lean_object* x_2, lean_object* x_3) {
+_start:
+{
+lean_object* x_4; 
+x_4 = lp_info_x2dgeometry_modularJ(x_1, x_2, x_3);
+lean_dec(x_3);
+lean_dec_ref(x_2);
+return x_4;
+}
+}
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_spectralEpsilon___redArg___lam__0(lean_object* x_1, lean_object* x_2) {
+_start:
+{
+uint8_t x_3; 
+x_3 = !lean_is_exclusive(x_2);
+if (x_3 == 0)
+{
+lean_object* x_4; lean_object* x_5; 
+x_4 = lean_ctor_get(x_2, 1);
+x_5 = lean_apply_1(x_1, x_4);
+lean_ctor_set(x_2, 1, x_5);
+return x_2;
+}
+else
+{
+lean_object* x_6; lean_object* x_7; lean_object* x_8; lean_object* x_9; 
+x_6 = lean_ctor_get(x_2, 0);
+x_7 = lean_ctor_get(x_2, 1);
+lean_inc(x_7);
+lean_inc(x_6);
+lean_dec(x_2);
+x_8 = lean_apply_1(x_1, x_7);
+x_9 = lean_alloc_ctor(0, 2, 0);
+lean_ctor_set(x_9, 0, x_6);
+lean_ctor_set(x_9, 1, x_8);
+return x_9;
+}
+}
+}
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_spectralEpsilon___redArg(lean_object* x_1) {
+_start:
+{
+lean_object* x_2; lean_object* x_3; lean_object* x_4; lean_object* x_5; 
+x_2 = lean_ctor_get(x_1, 1);
+x_3 = lp_mathlib_SubNegZeroMonoid_toNegZeroClass___redArg(x_2);
+x_4 = lean_ctor_get(x_3, 1);
+lean_inc(x_4);
+lean_dec_ref(x_3);
+x_5 = lean_alloc_closure((void*)(lp_info_x2dgeometry_spectralEpsilon___redArg___lam__0), 2, 1);
+lean_closure_set(x_5, 0, x_4);
+return x_5;
+}
+}
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_spectralEpsilon___redArg___boxed(lean_object* x_1) {
+_start:
+{
+lean_object* x_2; 
+x_2 = lp_info_x2dgeometry_spectralEpsilon___redArg(x_1);
+lean_dec_ref(x_1);
+return x_2;
+}
+}
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_spectralEpsilon(lean_object* x_1, lean_object* x_2, lean_object* x_3) {
+_start:
+{
+lean_object* x_4; 
+x_4 = lp_info_x2dgeometry_spectralEpsilon___redArg(x_2);
+return x_4;
+}
+}
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_spectralEpsilon___boxed(lean_object* x_1, lean_object* x_2, lean_object* x_3) {
+_start:
+{
+lean_object* x_4; 
+x_4 = lp_info_x2dgeometry_spectralEpsilon(x_1, x_2, x_3);
+lean_dec(x_3);
+lean_dec_ref(x_2);
+return x_4;
+}
+}
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_complexI___redArg(lean_object* x_1) {
+_start:
+{
+lean_object* x_2; lean_object* x_3; lean_object* x_4; 
+x_2 = lp_info_x2dgeometry_modularJ___closed__0;
+x_3 = lp_info_x2dgeometry_spectralEpsilon___redArg(x_1);
+x_4 = lp_mathlib_LinearMap_comp___redArg(x_2, x_3);
+return x_4;
+}
+}
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_complexI___redArg___boxed(lean_object* x_1) {
+_start:
+{
+lean_object* x_2; 
+x_2 = lp_info_x2dgeometry_complexI___redArg(x_1);
+lean_dec_ref(x_1);
+return x_2;
+}
+}
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_complexI(lean_object* x_1, lean_object* x_2, lean_object* x_3) {
+_start:
+{
+lean_object* x_4; 
+x_4 = lp_info_x2dgeometry_complexI___redArg(x_2);
+return x_4;
+}
+}
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_complexI___boxed(lean_object* x_1, lean_object* x_2, lean_object* x_3) {
+_start:
+{
+lean_object* x_4; 
+x_4 = lp_info_x2dgeometry_complexI(x_1, x_2, x_3);
+lean_dec(x_3);
+lean_dec_ref(x_2);
+return x_4;
+}
+}
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_clmComm___redArg___lam__0(lean_object* x_1, lean_object* x_2, lean_object* x_3, lean_object* x_4) {
+_start:
+{
+lean_object* x_5; lean_object* x_6; lean_object* x_7; lean_object* x_8; lean_object* x_9; lean_object* x_10; lean_object* x_11; uint8_t x_12; 
+x_5 = lean_ctor_get(x_1, 2);
+lean_inc(x_5);
+lean_dec_ref(x_1);
+lean_inc_ref(x_3);
+lean_inc_ref(x_2);
+x_6 = lp_mathlib_LinearMap_comp___redArg(x_2, x_3);
+lean_inc_ref(x_4);
+x_7 = lean_apply_1(x_6, x_4);
+x_8 = lean_ctor_get(x_7, 0);
+lean_inc(x_8);
+x_9 = lean_ctor_get(x_7, 1);
+lean_inc(x_9);
+lean_dec_ref(x_7);
+x_10 = lp_mathlib_LinearMap_comp___redArg(x_3, x_2);
+x_11 = lean_apply_1(x_10, x_4);
+x_12 = !lean_is_exclusive(x_11);
+if (x_12 == 0)
+{
+lean_object* x_13; lean_object* x_14; lean_object* x_15; lean_object* x_16; 
+x_13 = lean_ctor_get(x_11, 0);
+x_14 = lean_ctor_get(x_11, 1);
+lean_inc(x_5);
+x_15 = lean_apply_2(x_5, x_8, x_13);
+x_16 = lean_apply_2(x_5, x_9, x_14);
+lean_ctor_set(x_11, 1, x_16);
+lean_ctor_set(x_11, 0, x_15);
+return x_11;
+}
+else
+{
+lean_object* x_17; lean_object* x_18; lean_object* x_19; lean_object* x_20; lean_object* x_21; 
+x_17 = lean_ctor_get(x_11, 0);
+x_18 = lean_ctor_get(x_11, 1);
+lean_inc(x_18);
+lean_inc(x_17);
+lean_dec(x_11);
+lean_inc(x_5);
+x_19 = lean_apply_2(x_5, x_8, x_17);
+x_20 = lean_apply_2(x_5, x_9, x_18);
+x_21 = lean_alloc_ctor(0, 2, 0);
+lean_ctor_set(x_21, 0, x_19);
+lean_ctor_set(x_21, 1, x_20);
+return x_21;
+}
+}
+}
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_clmComm___redArg(lean_object* x_1, lean_object* x_2, lean_object* x_3) {
+_start:
+{
+lean_object* x_4; lean_object* x_5; 
+x_4 = lean_ctor_get(x_1, 1);
+lean_inc_ref(x_4);
+lean_dec_ref(x_1);
+x_5 = lean_alloc_closure((void*)(lp_info_x2dgeometry_clmComm___redArg___lam__0), 4, 3);
+lean_closure_set(x_5, 0, x_4);
+lean_closure_set(x_5, 1, x_2);
+lean_closure_set(x_5, 2, x_3);
+return x_5;
+}
+}
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_clmComm(lean_object* x_1, lean_object* x_2, lean_object* x_3, lean_object* x_4, lean_object* x_5) {
+_start:
+{
+lean_object* x_6; 
+x_6 = lp_info_x2dgeometry_clmComm___redArg(x_2, x_4, x_5);
+return x_6;
+}
+}
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_clmComm___boxed(lean_object* x_1, lean_object* x_2, lean_object* x_3, lean_object* x_4, lean_object* x_5) {
+_start:
+{
+lean_object* x_6; 
+x_6 = lp_info_x2dgeometry_clmComm(x_1, x_2, x_3, x_4, x_5);
+lean_dec(x_3);
+return x_6;
+}
+}
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_hessianIndefiniteForm___redArg(lean_object* x_1, lean_object* x_2, lean_object* x_3) {
+_start:
+{
+lean_object* x_4; lean_object* x_5; lean_object* x_6; lean_object* x_7; lean_object* x_8; lean_object* x_9; lean_object* x_10; lean_object* x_11; 
+x_4 = lean_ctor_get(x_1, 1);
+lean_inc(x_4);
+lean_dec_ref(x_1);
+x_5 = lean_ctor_get(x_2, 0);
+lean_inc(x_5);
+x_6 = lean_ctor_get(x_2, 1);
+lean_inc(x_6);
+lean_dec_ref(x_2);
+x_7 = lean_ctor_get(x_3, 0);
+lean_inc(x_7);
+x_8 = lean_ctor_get(x_3, 1);
+lean_inc(x_8);
+lean_dec_ref(x_3);
+lean_inc(x_4);
+x_9 = lean_apply_2(x_4, x_5, x_8);
+x_10 = lean_apply_2(x_4, x_7, x_6);
+x_11 = lean_alloc_closure((void*)(lp_mathlib_Real_definition___lam__0_00___x40_Mathlib_Data_Real_Basic_1138242547____hygCtx___hyg_8_), 3, 2);
+lean_closure_set(x_11, 0, x_9);
+lean_closure_set(x_11, 1, x_10);
+return x_11;
+}
+}
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_hessianIndefiniteForm(lean_object* x_1, lean_object* x_2, lean_object* x_3, lean_object* x_4, lean_object* x_5) {
+_start:
+{
+lean_object* x_6; 
+x_6 = lp_info_x2dgeometry_hessianIndefiniteForm___redArg(x_3, x_4, x_5);
+return x_6;
+}
+}
+LEAN_EXPORT lean_object* lp_info_x2dgeometry_hessianIndefiniteForm___boxed(lean_object* x_1, lean_object* x_2, lean_object* x_3, lean_object* x_4, lean_object* x_5) {
+_start:
+{
+lean_object* x_6; 
+x_6 = lp_info_x2dgeometry_hessianIndefiniteForm(x_1, x_2, x_3, x_4, x_5);
+lean_dec_ref(x_2);
+return x_6;
+}
+}
 lean_object* initialize_Init(uint8_t builtin);
 lean_object* initialize_info_x2dgeometry_InfoGeometry_Basic(uint8_t builtin);
 lean_object* initialize_mathlib_Mathlib(uint8_t builtin);
+lean_object* initialize_mathlib_Mathlib_Probability_Distributions_Poisson(uint8_t builtin);
 static bool _G_initialized = false;
 LEAN_EXPORT lean_object* initialize_info_x2dgeometry_InfoGeometry(uint8_t builtin) {
 lean_object * res;
@@ -62,6 +372,11 @@ lean_dec_ref(res);
 res = initialize_mathlib_Mathlib(builtin);
 if (lean_io_result_is_error(res)) return res;
 lean_dec_ref(res);
+res = initialize_mathlib_Mathlib_Probability_Distributions_Poisson(builtin);
+if (lean_io_result_is_error(res)) return res;
+lean_dec_ref(res);
+lp_info_x2dgeometry_modularJ___closed__0 = _init_lp_info_x2dgeometry_modularJ___closed__0();
+lean_mark_persistent(lp_info_x2dgeometry_modularJ___closed__0);
 return lean_io_result_mk_ok(lean_box(0));
 }
 #ifdef __cplusplus

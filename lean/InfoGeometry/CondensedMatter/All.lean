b00bbfaa748d1284f2c283bfb4a15193d6e9ce21
nickgou-nuke <177388865+nickgou-nuke@users.noreply.github.com>
2026-06-12 07:14:32 +0300

    feat: add nonorientable Weyl mod two bridge

--- lean/InfoGeometry/CondensedMatter/All.lean
+++ lean/InfoGeometry/CondensedMatter/All.lean
@@ -1,5 +1,6 @@
 import InfoGeometry.CondensedMatter.DIIISuperfluid
 import InfoGeometry.CondensedMatter.CliffordAtomsZ2n
+import InfoGeometry.CondensedMatter.NonOrientableWeylSemimetal
 
 namespace InfoGeometry.CondensedMatter
 

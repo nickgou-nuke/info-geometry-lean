19e815b82dfb4faeb31df91fe49a3341beb3d875
nickgou-nuke <177388865+nickgou-nuke@users.noreply.github.com>
2026-06-13 16:50:28 +0300

    feat: formalize finite Klein Berry connection shadow

--- lean/InfoGeometry/OperatorAlgebra/All.lean
+++ lean/InfoGeometry/OperatorAlgebra/All.lean
@@ -91,6 +91,7 @@ import InfoGeometry.OperatorAlgebra.IndividuatedCayley
 import InfoGeometry.OperatorAlgebra.InformationAffineKacMoodyBridge
 import InfoGeometry.OperatorAlgebra.JonesCalibration
 import InfoGeometry.OperatorAlgebra.JUnitaryTopologicalCharge
+import InfoGeometry.OperatorAlgebra.KleinBerryConnectionFinite
 import InfoGeometry.External.Virasoro.AffineKacMoody
 import InfoGeometry.OperatorAlgebra.KacMoodyAlgebraicLemmas
 import InfoGeometry.OperatorAlgebra.KANLightConeAffineBridge

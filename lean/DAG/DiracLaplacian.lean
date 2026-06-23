4b65a516289a7539c73821503154d6cd38f17cf0
nickgou-nuke <177388865+nickgou-nuke@users.noreply.github.com>
2026-06-23 06:26:20 +0300

    Fix tokenization issues and refactor Bost-Connes modular flow in Lean 4

--- lean/DAG/DiracLaplacian.lean
+++ lean/DAG/DiracLaplacian.lean
@@ -32,7 +32,7 @@ theorem dirac_sq_upper_left_is_laplacian0_chain :
     let D := graphDirac chainComplex
     let Dsq := matMul D D
     let Δ₀ := laplacian0 chainComplex
-    Dsq[0]![0]! = Δ₀[0]![0]! := by
+    (Dsq[0]!)[0]! = (Δ₀[0]!)[0]! := by
   native_decide
 
 /-- The lower-right entry of `D²` agrees with the down-Laplacian on 1-chains. -/
@@ -40,21 +40,21 @@ theorem dirac_sq_lower_right_is_down_laplacian1_chain :
     let D := graphDirac chainComplex
     let Dsq := matMul D D
     let downΔ₁ := matMul (boundary1 chainComplex) (matTranspose (boundary1 chainComplex))
-    Dsq[3]![3]! = downΔ₁[0]![0]! := by
+    (Dsq[3]!)[3]! = (downΔ₁[0]!)[0]! := by
   native_decide
 
 /-- The upper-right off-diagonal block of `D²` vanishes on the chain. -/
 theorem dirac_sq_upper_right_is_zero_chain :
     let D := graphDirac chainComplex
     let Dsq := matMul D D
-    Dsq[0]![3]! = 0 := by
+    (Dsq[0]!)[3]! = 0 := by
   native_decide
 
 /-- The lower-left off-diagonal block of `D²` vanishes on the chain. -/
 theorem dirac_sq_lower_left_is_zero_chain :
     let D := graphDirac chainComplex
     let Dsq := matMul D D
-    Dsq[3]![0]! = 0 := by
+    (Dsq[3]!)[0]! = 0 := by
   native_decide
 
 /-- The trace of `D²` is the sum of the two diagonal Laplacian block traces. -/

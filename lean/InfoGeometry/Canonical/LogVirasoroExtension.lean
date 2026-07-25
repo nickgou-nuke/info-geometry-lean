a38998542a78fe1f44e8c7a3eed8e89ef5ed3231
Nikolay Goutev <nikolay.v.goutev@gmail.com>
2026-07-25 12:19:55 +0300

    fix: update map_lie with LieHom.map_lie in LogVirasoroExtension.lean

--- lean/InfoGeometry/Canonical/LogVirasoroExtension.lean
+++ lean/InfoGeometry/Canonical/LogVirasoroExtension.lean
@@ -101,7 +101,7 @@ def makeLogVirasoroRepresentation
     intro x y
     change blockOp (ρ ⁅x, y⁆) (c ⁅x, y⁆) = (blockOp (ρ x) (c x)).commutator (blockOp (ρ y) (c y))
     rw [blockOp_commutator]
-    have h1 : ρ ⁅x, y⁆ = (ρ x).commutator (ρ y) := map_lie ρ x y
+    have h1 : ρ ⁅x, y⁆ = (ρ x).commutator (ρ y) := LieHom.map_lie ρ x y
     have h2 : c ⁅x, y⁆ = (ρ x).commutator (c y) + (c x).commutator (ρ y) := hc x y
     rw [h1, h2]
 

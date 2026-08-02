94b8d9b516fbb25b241cba07c18776ec9af78d20
nickgou-nuke <nikolay.v.goutev@gmail.com>
2026-08-02 07:34:00 +0300

    chore(analysis): export Mellin-wavelet scale-shape bridge

--- lean/InfoGeometry/Analysis/All.lean
+++ lean/InfoGeometry/Analysis/All.lean
@@ -5,6 +5,7 @@ import InfoGeometry.Analysis.CliffordWaveletDonohoStark
 import InfoGeometry.Analysis.D23HurwitzCliffordFilterBank
 import InfoGeometry.Analysis.DiscreteHurwitzCliffordWavelet
 import InfoGeometry.Analysis.LaplaceMellinScaleShapeTransform
+import InfoGeometry.Analysis.MellinWaveletScaleShapeBridge
 import InfoGeometry.Analysis.MellinZetaScaling
 import InfoGeometry.Analysis.FiniteSpectralMellinTaylor
 import InfoGeometry.Analysis.FiniteSpectralHeatMellin

9d317301110a49afe5e68df963ee07c947273a4c
Nikolay Goutev <nikolay.v.goutev@gmail.com>
2026-08-06 07:35:28 +0300

    feat: implement 0-sorry ConeAction for Cartan metric

--- lean/InfoGeometry/Riemannian/ConeAction.lean
+++ lean/InfoGeometry/Riemannian/ConeAction.lean
@@ -19,27 +19,46 @@ def coneConjugationAction (G : ClPlus Q) (X : ClPlus Q) : ClPlus Q :=
   L_action Q G (R_action Q (hestenesAdjoint Q v0 G) X)
 
 /-- hestenesAdjoint на идентитета е идентитетът. -/
-theorem hestenesAdjoint_one : hestenesAdjoint Q v0 (1 : ClPlus Q) = 1 := by
-  sorry
+theorem hestenesAdjoint_one (hv0_norm : Q v0 = 1) : hestenesAdjoint Q v0 (1 : ClPlus Q) = 1 := by
+  apply Subtype.ext
+  dsimp [hestenesAdjoint]
+  rw [reverse.map_one, mul_one]
+  exact gamma0_sq Q v0 hv0_norm
 
 /-- Доказателство, че идентитетът действа тривиално: 1 ⬝ X = X -/
-theorem coneAction_one (X : ClPlus Q) : coneConjugationAction Q v0 1 X = X := by
-  sorry
+theorem coneAction_one (hv0_norm : Q v0 = 1) (X : ClPlus Q) : coneConjugationAction Q v0 1 X = X := by
+  dsimp [coneConjugationAction]
+  apply Subtype.ext
+  dsimp [L_action, R_action]
+  have h_adj : (hestenesAdjoint Q v0 (1 : ClPlus Q)).val = 1 := congrArg Subtype.val (hestenesAdjoint_one Q v0 hv0_norm)
+  rw [h_adj, mul_one, one_mul]
 
 /-- hestenesAdjoint обръща реда на умножение (анти-автоморфизъм). -/
-theorem hestenesAdjoint_mul (G1 G2 : ClPlus Q) :
+theorem hestenesAdjoint_mul (hv0_norm : Q v0 = 1) (G1 G2 : ClPlus Q) :
     hestenesAdjoint Q v0 (G1 * G2) = hestenesAdjoint Q v0 G2 * hestenesAdjoint Q v0 G1 := by
-  sorry
+  apply Subtype.ext
+  dsimp [hestenesAdjoint]
+  rw [reverse.map_mul]
+  have hsq := gamma0_sq Q v0 hv0_norm
+  simp only [mul_assoc]
+  congr 1
+  congr 1
+  rw [← mul_assoc (gamma0 Q v0) (gamma0 Q v0), hsq, one_mul]
 
 /-- Доказателство за свойството на съвместимост: (G1 * G2) ⬝ X = G1 ⬝ (G2 ⬝ X) -/
-theorem coneAction_mul (G1 G2 : ClPlus Q) (X : ClPlus Q) :
+theorem coneAction_mul (hv0_norm : Q v0 = 1) (G1 G2 : ClPlus Q) (X : ClPlus Q) :
     coneConjugationAction Q v0 (G1 * G2) X = coneConjugationAction Q v0 G1 (coneConjugationAction Q v0 G2 X) := by
-  sorry
+  apply Subtype.ext
+  dsimp [coneConjugationAction, L_action, R_action]
+  have h_adj := congrArg Subtype.val (hestenesAdjoint_mul Q v0 hv0_norm G1 G2)
+  dsimp at h_adj
+  rw [h_adj]
+  simp only [mul_assoc]
 
 /-- Официално регистриране на действието като MulAction в Lean 4 типажите -/
-instance : MulAction (ClPlus Q) (ClPlus Q) where
+instance (hv0_norm : Q v0 = 1) : MulAction (ClPlus Q) (ClPlus Q) where
   smul := coneConjugationAction Q v0
-  one_smul := coneAction_one Q v0
-  mul_smul := coneAction_mul Q v0
+  one_smul := coneAction_one Q v0 hv0_norm
+  mul_smul := coneAction_mul Q v0 hv0_norm
 
 end InfoGeometry.Riemannian

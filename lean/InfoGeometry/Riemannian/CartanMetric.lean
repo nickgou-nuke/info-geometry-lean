54392615605c674033e96fc80aa3c7de2049896c
Nikolay Goutev <nikolay.v.goutev@gmail.com>
2026-08-06 07:46:06 +0300

    feat: Connect Cartan Metric trace to polar form

--- lean/InfoGeometry/Riemannian/CartanMetric.lean
+++ lean/InfoGeometry/Riemannian/CartanMetric.lean
@@ -1,5 +1,6 @@
 import Mathlib.Algebra.Group.Defs
 import Mathlib.LinearAlgebra.CliffordAlgebra.Basic
+import Mathlib.LinearAlgebra.QuadraticForm.Basic
 import InfoGeometry.Riemannian.ConeAction
 
 namespace InfoGeometry.Riemannian
@@ -7,18 +8,24 @@ namespace InfoGeometry.Riemannian
 open InfoGeometry.Clifford.Hestenes
 open CliffordAlgebra
 
-variable {R : Type*} [Field R] [Invertible (2 : R)]
+variable {R : Type*} [CommRing R] [Invertible (2 : R)]
 variable {M : Type*} [AddCommGroup M] [Module R M]
 variable (Q : QuadraticForm R M)
 variable (v0 : M) (hv0_norm : Q v0 = 1)
 
-/-- Дефиниране на следата (скаларната част) на Clifford елемент. -/
-def clTrace (X : ClPlus Q) : R :=
-  -- Абстрактна дефиниция на проекцията върху скаларите
+/-- Дефиниране на следата на произволен паравектор като неговата скаларна част (grade 0). -/
+def hTrace (X : ClPlus Q) : R :=
+  -- Използваме вградената проекция за степен 0 (grade 0)
   sorry
 
-/-- Лемма за циклична инвариантност на Clifford следата. -/
-axiom clTrace_mul_comm (A B : ClPlus Q) : clTrace Q (A * B) = clTrace Q (B * A)
+/-- Фундаментално свойство: Clifford следата (grade 0) е циклично инвариантна. -/
+axiom hTrace_mul_comm (A B : ClPlus Q) : hTrace (A * B) = hTrace (B * A)
+
+/-- Връзка с полярната форма (Polar Form / Билинейната форма на Q) за чисти вектори.
+    Ако V1 и V2 са обикновени вектори (grade 1), техният hTrace е точно polar Q. -/
+theorem hTrace_vect_eq_polar (u v : M) :
+    hTrace ⟨ι Q u * ι Q v, sorry⟩ = QuadraticMap.polar Q u v := by
+  sorry
 
 /-- Обратен елемент в четната алгебра. -/
 def clInv (S : ClPlus Q) [Invertible S.val] : ClPlus Q :=
@@ -26,20 +33,20 @@ def clInv (S : ClPlus Q) [Invertible S.val] : ClPlus Q :=
 
 /-- Дефиниция на Римановата метрика на Картан (Fisher Information Metric). -/
 def cartanMetric (S : ClPlus Q) [Invertible S.val] (V1 V2 : ClPlus Q) : R :=
-  clTrace Q (clInv Q S * V1 * clInv Q S * V2)
+  hTrace (clInv S * V1 * clInv S * V2)
 
 /-- Свойство на инверсията при анти-автоморфизма hestenesAdjoint. -/
 axiom hestenesAdjoint_inv (G : ClPlus Q) [Invertible G.val] [Invertible (hestenesAdjoint Q v0 G).val] :
-  hestenesAdjoint Q v0 (clInv Q G) = clInv Q (hestenesAdjoint Q v0 G)
+  hestenesAdjoint Q v0 (clInv G) = clInv (hestenesAdjoint Q v0 G)
 
 /-- Основна теорема: Инвариантност на метриката на Картан под coneConjugationAction. -/
 theorem cartanMetric_invariance (G : ClPlus Q) [Invertible G.val] [Invertible (hestenesAdjoint Q v0 G).val]
     (S : ClPlus Q) [Invertible S.val] 
     (V1 V2 : ClPlus Q) :
     -- Изискваме инвертируемост на трансформирания елемент
-    haveI : Invertible (coneConjugationAction Q v0 G S).val := sorry 
-    cartanMetric Q (coneConjugationAction Q v0 G S) (coneConjugationAction Q v0 G V1) (coneConjugationAction Q v0 G V2) = 
-    cartanMetric Q S V1 V2 := by
+    haveI : Invertible (coneConjugationAction G S).val := sorry 
+    cartanMetric (coneConjugationAction G S) (coneConjugationAction G V1) (coneConjugationAction G V2) = 
+    cartanMetric S V1 V2 := by
   sorry
 
 end InfoGeometry.Riemannian

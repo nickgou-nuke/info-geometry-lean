73c4acf86d2fd6e257c9f4a77c8ee622c4033e03
nickgou-nuke <nikolay.v.goutev@gmail.com>
2026-08-23 20:13:10 +0300

    Refine twistor polarization import surface

--- lean/InfoGeometry/Twistor/All.lean
+++ lean/InfoGeometry/Twistor/All.lean
@@ -64,7 +64,7 @@ import InfoGeometry.Twistor.ProjectiveNullUnorderedConfiguration
 import InfoGeometry.Twistor.PenroseTwistor
 import InfoGeometry.Twistor.PenroseIncidence
 import InfoGeometry.Twistor.RealSplitOctonionCarrierBridge
-import InfoGeometry.Twistor.ChiralTwistorSheets
+import InfoGeometry.Twistor.ChiralTwistorPeirceSheets
 import InfoGeometry.Twistor.ChiralTwistorZornCoupling
 import InfoGeometry.Twistor.RollingSpinorMobiusBridge
 import InfoGeometry.Twistor.PhaseNativePluckerBridge

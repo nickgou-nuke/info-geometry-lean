be75e79ff5d1317aea1d6781844d876e7d56d979
nickgou-nuke <nikolay.v.goutev@gmail.com>
2026-09-02 14:08:15 +0300

    Register compiled lambda braid Hestenes bridge

--- lean/InfoGeometry/Categorical/All.lean
+++ lean/InfoGeometry/Categorical/All.lean
@@ -1,6 +1,7 @@
 import InfoGeometry.Categorical.Holonomy
 import InfoGeometry.Categorical.FilteredColimitExactness
 import InfoGeometry.Categorical.HadjiivanovBraidGroupColimit
+import InfoGeometry.Categorical.LambdaBraidHestenesKreinBridge
 import InfoGeometry.Categorical.TensorColimit
 import InfoGeometry.Categorical.CuntzCantorKMSColimitBridge
 import InfoGeometry.Categorical.FiniteWordPathWeights

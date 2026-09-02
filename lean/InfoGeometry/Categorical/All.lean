2327cf5230285f621df28a32e6633e00b858e5ad
nickgou-nuke <nikolay.v.goutev@gmail.com>
2026-09-02 15:03:37 +0300

    Register braid Hestenes-Krein Virasoro bridge

--- lean/InfoGeometry/Categorical/All.lean
+++ lean/InfoGeometry/Categorical/All.lean
@@ -2,6 +2,7 @@ import InfoGeometry.Categorical.Holonomy
 import InfoGeometry.Categorical.FilteredColimitExactness
 import InfoGeometry.Categorical.HadjiivanovBraidGroupColimit
 import InfoGeometry.Categorical.LambdaBraidHestenesKreinBridge
+import InfoGeometry.Categorical.BraidHestenesKreinVirasoroBridge
 import InfoGeometry.Categorical.TensorColimit
 import InfoGeometry.Categorical.CuntzCantorKMSColimitBridge
 import InfoGeometry.Categorical.FiniteWordPathWeights

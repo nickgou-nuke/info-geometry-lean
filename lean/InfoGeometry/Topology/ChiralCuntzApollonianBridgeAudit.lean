89250a35595060afcd52cc3dbed41009d87ba47e
Nikolay Goutev <nikolay.v.goutev@gmail.com>
2026-09-10 19:45:28 +0300

    refactor(topology): deduplicate ChiralCuntzApollonianBridge by wiring to canonical owners

--- lean/InfoGeometry/Topology/ChiralCuntzApollonianBridgeAudit.lean
+++ lean/InfoGeometry/Topology/ChiralCuntzApollonianBridgeAudit.lean
@@ -17,14 +17,4 @@ open InfoGeometry.Topology.ChiralCuntzApollonian
 #print axioms wordMeasure_sum_children
 #print axioms conformal_measure_preservation_cons
 #print axioms conformal_measure_preservation_nil
-#print axioms cuntzMarkovStep_one
-#print axioms cuntzMarkovStep_cylinder
-#print axioms Q_plus_O2_sq_zero
-#print axioms Q_minus_O2_sq_zero
-#print axioms O2_anticommutator_eq_one
-#print axioms brst_filtering
-#print axioms weylScale_add
-#print axioms weylScale_horizon_eq
-#print axioms dilaton_tendsto_atTop
-#print axioms weylScale_horizon_tendsto_atTop
 #print axioms certified_chiral_cuntz_apollonian_bridge
